7f5c2a688643d6011ac4841380856751d26227a1
Alexander Schwartz <aschwart@redhat.com>
2024-03-27 19:31:50 +0100

    Review changes
    * Added specific images
    * switched from start-dev to start
    * removed a realm which is not imported as a new master realm is created at startup

--- test.sh
+++ test.sh
@@ -1,10 +1,9 @@
-
 #!/bin/sh
 num_times=$1
 start_time=$(date +%s)
 for ((i=1; i<=$num_times; i++))
 do
-    curl -s -X POST -H "Authorization:Basic b3BmYWItY2xpZW50Om9wZmFiLWtleWNsb2FrLXNlY3JldA==" -d "username=admin&password=test&grant_type=password&client_id=opfab-client" localhost:89/auth/realms/dev/protocol/openid-connect/token >> /dev/null
+    curl -s -X POST -H "Authorization:Basic b3BmYWItY2xpZW50Om9wZmFiLWtleWNsb2FrLXNlY3JldA==" -d "username=admin&password=test&grant_type=password&client_id=opfab-client" localhost:8080/auth/realms/dev/protocol/openid-connect/token >> /dev/null
 done
 end_time=$(date +%s)
 echo "Number of token requests: $num_times"
